e6608c64aae5aa47e4075d832ba81d53224a16be
Joseph Lau <josephlausf@gmail.com>
2015-10-02 16:01:40 -0700

    initial commit

---
+++ BentoDriver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BentoDriver
+//
+//  Created by Joseph Lau on 10/2/15.
+//  Copyright © 2015 Joseph Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
