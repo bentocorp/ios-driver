16a07ff38478d5f2c47137a07a30e4c4d00d0a48
Joseph Lau <josephlausf@gmail.com>
2015-10-09 18:07:54 -0700

    added classes

---
+++ BentoDriver/OrderListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OrderListViewController.swift
+//  BentoDriver
+//
+//  Created by Joseph Lau on 10/9/15.
+//  Copyright © 2015 Joseph Lau. All rights reserved.
+//
+
+import UIKit
+
+class OrderListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BentoDriver/SocketHandler.swift
@@ -0,0 +1,9 @@
+//
+//  SocketHandler.swift
+//  BentoDriver
+//
+//  Created by Joseph Lau on 10/9/15.
+//  Copyright © 2015 Joseph Lau. All rights reserved.
+//
+
+import Foundation
