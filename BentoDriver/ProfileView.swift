2feb36e5264c868e05e28007560215d537c65d4d
Joseph Lau <josephlausf@gmail.com>
2015-10-09 18:10:00 -0700

    added all classes

---
+++ BentoDriver/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  BentoDriver
+//
+//  Created by Joseph Lau on 10/9/15.
+//  Copyright © 2015 Joseph Lau. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
