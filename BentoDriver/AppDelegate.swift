af1c950b44d3e635d76686040e66d3e598209ee4
Joseph Lau <josephlausf@gmail.com>
2015-10-09 18:03:14 -0700

    added UIBackgroundTaskIdentifier in appdelete for testing background execution

--- BentoDriver/AppDelegate.swift
+++ BentoDriver/AppDelegate.swift
@@ -11,13 +11,13 @@ import Socket_IO_Client_Swift
 import SwiftyJSON
 
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    UIBackgroundTaskIdentifier bgTask
-    
     var window: UIWindow?
     let socket = SocketIOClient(socketURL: "http://54.191.141.101:8081", opts: nil)
-
+    var bgTask: UIBackgroundTaskIdentifier = 0
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -74,10 +74,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        
+        self.bgTask = application.beginBackgroundTaskWithExpirationHandler({ () -> Void in
+            self.bgTask = UIBackgroundTaskInvalid
+        })
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
+        application.endBackgroundTask(self.bgTask)
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
